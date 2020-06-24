a3a5ab0e297fc9d0bf4ab10b8e599393a570444d
lesylanne <lesyl.adriano@gmail.com>
2020-06-24 18:45:57 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-project';
+    title = 'Hi Lesyl Anne!';
+    myApp = "This is my app"
 }
